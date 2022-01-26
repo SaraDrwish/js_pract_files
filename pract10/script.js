6ef89227e535e19bc5fbee6120d78f1793754609
Sara <38770404+SaraDrwish@users.noreply.github.com>
2022-01-26 21:46:18 +0300

    ginal js

--- pract10/script.js
+++ pract10/script.js
@@ -1,19 +1,36 @@
 
-let filterlist = document.querySelector(".filtetTop ul li");
+let filterlist = document.querySelectorAll(".filtetTop ul li");  // alllllllllllllllllll foreach must be like thisss
 let img = Array.from(document.images);
 // document.querySelectorAll(".filterpic img");
 
 //lli.forEach( (li)=> {   })
 
- filterlist.forEach( (item)=> {
+  filterlist.forEach((item)=> {
 
-         item.addEventListener("click", function removmain() {
-             filterlist.forEach((item) => {
+    item.addEventListener("click" ,  removmain ) ;
+    item.addEventListener("click" ,  managimgs );
 
-                 item.classList.remove("main");
-                 this.classList.add("main");
+     });
 
-             });
-         });
+     function removmain() {
 
-     } );
+        filterlist.forEach((item) => {
+
+            item.classList.remove("main");
+            this.classList.add("main");
+
+        });
+     }
+
+     function managimgs(){
+
+        img.forEach( (mg)=>{
+            mg.style.display = "none";
+        } );
+
+        document.querySelectorAll(this.dataset.type).forEach( (d)=>{
+            d.style.display = "block";
+        } )
+
+
+     }
